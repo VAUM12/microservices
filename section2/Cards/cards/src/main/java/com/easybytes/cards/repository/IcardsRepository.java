b75de66842796d6ed56dc6e3a6783c55a9021223
meets <meets@monovative.com>
2024-11-05 16:01:35 +0530

    initial commit of card

--- section2/Cards/cards/src/main/java/com/easybytes/cards/repository/IcardsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.easybytes.cards.repository;
-
-import com.easybytes.cards.entity.Cards;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IcardsRepository extends JpaRepository<Cards, Long> {
-}
--- section2/Cards/cards/src/main/java/com/easybytes/cards/service/impl/IcardsServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.easybytes.cards.service.impl;
-
-import com.easybytes.cards.service.IcardsService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class IcardsServiceImpl implements IcardsService {
-
-}
